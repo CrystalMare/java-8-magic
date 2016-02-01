408ed2ea1a581c7bf0a5ef876640e276e14303d0
Sven Olderaan <admin@heaven-craft.net>
2016-02-01 14:39:48 +0100

    Added comment

--- src/main/java/nl/ireal/lambda/CodingWithoutNullchecks.java
+++ src/main/java/nl/ireal/lambda/CodingWithoutNullchecks.java
@@ -7,6 +7,7 @@ public class CodingWithoutNullchecks {
     /*
     The main reason you would use is is to indicate that a given method CAN return null so it can be used in streams.
     Java <7 has no way of showing that an object can be null.
+    Surely, you can add your own way of doing this. But this is a universal way of doing it with lambda
     With the introduction of lamda and functional interfaces, this can be made easy.
     More on this topic:
     http://www.deadcoderising.com/2015-10-06-java-8-removing-null-checks-with-optional/
